34b14c7745b9c98af2c4074a345e092be437cfcc
Leslie Moses <LesMoses@gmail.com>
2017-02-27 19:34:09 -0500

    trivia 0227 730p

---
+++ trivia bank.js
@@ -0,0 +1,142 @@
+	cool image look:
+
+
+ 
+Img {
+   border: 15px solid #3e2b14;
+  padding: 10px;
+}
+
+
+  
+		<div class="container">
+	<div class="row">
+	 
+		  
+		  <div class="col-sm-6 col-lg-2">
+		   </div>
+		   <br>
+		  <br>
+		  <div class="panel-heading col-sm-6 col-lg-8"><h1 class="quote"><strong>Do not say that the darkness of the prison has xxxxxxxx you, for your shackles are wings—wings of flight into the heart and soul of humanity.</strong></h1><br>  
+		  <br>
+		  <br>
+		  <label class="choice">&nbsp;&nbsp;<input  type="radio">shackled</label>
+		  &nbsp;&nbsp;<label class="choice"><input type="radio">cleansed</label>
+		  &nbsp;&nbsp;<label class="choice"><input type="radio">cut </label>
+		</div>
+		   
+		   <div class="col-sm-3 col-lg-2">
+		    <div class="thumbnail">
+		      <img src="assets/images/matheson.jpg" style=width:150px; alt="...">
+		      <div class="caption">
+		        <h3>George Matheson</h3>
+		        <p>Scottish minister and hymn writer (“O Love That Will Not Let Me Go”)</p>
+		      </div>
+		    </div>
+		  </div>
+	 
+		  </div>
+		  </div>
+
+
+
+
+		  
+
+<div class="panel panel-default">
+		  <!-- Default panel contents -->
+		  <div class="panel-heading">George Matheson, Scottish minister and hymn writer (“O Love That Will Not Let Me Go”)
+	 
+</div>
+		  <div class="panel-body">
+		    <p>Do not say that the darkness of the prison has xxxxxxxx you, for your shackles are wings—wings of flight into the heart and soul of humanity.</p>
+		  </div>
+
+		  <!-- List group -->
+		  <ol class="list-group">
+		    <li class="list-group-item">shackled</li>
+		    <li class="list-group-item">cleansed</li>
+		    <li class="list-group-item">cut</li>
+		  </ol>
+		</div>
+
+		<div class="panel panel-default">
+		  <!-- Default panel contents -->
+		  <label class="panel-heading"><h2>Oswald Chambers</h2> evangelist and teacher, best known for “My Utmost for His Highest” devotional 
+
+		</label>
+		  <div class="panel-body">
+		    <h3>I feel sorry for the Christian who doesn’t have something in the circumstances of his life that he wishes xxxxx xxxxx xxxxx.</h3>
+		  </div>
+
+		  <!-- List group -->
+		  <ol class="list-group">
+		    <li class="list-group-item"><input type="checkbox">upon his friends.</li>
+		    <label class="list-group-item">were not there.</label>
+		    <li class="list-group-item">made him rich.
+		  </ol>
+		</div>
+
+
+	<div class="row">
+		  <div class="col-sm-12 col-lg-2">
+		    <div class="thumbnail">
+		      <img src="assets/images/eliot" alt="...">
+		      <div class="caption">
+		        <h3>Thumbnail label</h3>
+		        <p>...</p>
+		        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+		      </div>
+		    </div>
+		  </div>
+		
+
+		  <div class="col-sm-12 col-lg-2">
+		    <div class="thumbnail">
+		      <img src="..." alt="...">
+		      <div class="caption">
+		        <h3>Thumbnail label</h3>
+		        <p>...</p>
+		        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+
+		  <div class="col-sm-12 col-lg-2">
+		    <div class="thumbnail">
+		      <img src="..." alt="...">
+		      <div class="caption">
+		        <h3>Thumbnail label</h3>
+		        <p>...</p>
+		        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+
+		  <div class="col-sm-12 col-lg-2">
+		    <div class="thumbnail">
+		      <img src="..." alt="...">
+		      <div class="caption">
+		        <h3>Thumbnail label</h3>
+		        <p>...</p>
+		        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+
+		  <div class="col-sm-12 col-lg-2">
+		    <div class="thumbnail">
+		      <img src="..." alt="...">
+		      <div class="caption">
+		        <h3>Thumbnail label</h3>
+		        <p>...</p>
+		        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+
+	  </div>
